a17ba8037164d5760c07c6085a20cd34b353cd60
tri_dev <phanminh.tri@quocviet.com.vn>
2025-05-06 20:35:46 +0700

    remove color

--- lib/src/data_table_2.dart
+++ lib/src/data_table_2.dart
@@ -1177,7 +1177,6 @@ class DataTable2 extends DataTable {
                 decoration: decoration ?? theme.dataTableTheme.decoration,
                 child: Material(
                     type: MaterialType.transparency,
-                    color: Colors.transparent,
                     borderRadius: border?.borderRadius,
                     clipBehavior: clipBehavior,
                     child: rows.isEmpty
