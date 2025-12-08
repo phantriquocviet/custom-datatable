12dbb992cf2187b441f19d7c8c2e7a00728c74be
tri_dev <phanminh.tri@quocviet.com.vn>
2025-12-08 23:53:38 +0700

    Fixed: double tap checkbox

--- lib/src/data_table_2.dart
+++ lib/src/data_table_2.dart
@@ -963,7 +963,9 @@ class DataTable2 extends DataTable {
                     onRowSecondaryTapUp:
                         row is DataRow2 ? row.onSecondaryTapUp : null,
                     onSelectChanged: row.onSelectChanged != null
-                        ? () => row.onSelectChanged!(!row.selected)
+                        ? () => row is DataRow2
+                            ? row.onSelectChanged!(!row.isChecked)
+                            : row.onSelectChanged!(!row.selected)
                         : null,
                     overlayColor: row.color ?? effectiveDataRowColor);
 
